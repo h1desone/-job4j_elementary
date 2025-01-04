a91ae3a8e75a75272aed6ff53b476ecde6f1aed1
avot2 <avot201@gmail.com>
2025-01-04 18:26:56 +0300

    3.2. загрузка файлов на githud [#243564]

---
+++ src/main/java/ru/job4j/Godzilla.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Godzilla {
+    public static void main(String[] args) {
+        System.out.println("BAZZILAAA");
+        System.out.println("30.04.1945");
+    }
+}
